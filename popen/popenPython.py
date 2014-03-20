b7baaf0c7b0027b0c2c22cd2b08c661f3460bc8f
JunneJun <yangjun03@baidu.com>
2014-03-21 00:58:00 +0800

    update

--- popen/popenPython.py
+++ popen/popenPython.py
@@ -10,7 +10,7 @@ cmd ="""ps -ef | grep import_poi |  grep -v grep | wc -l"""
 logging.basicConfig(filename='time.log', format="%(levelname)-10s %(asctime)s %(name)s %(message)s", level=logging.DEBUG)
 
 #使用popen调用系统shell命令监控简称状态，进程停止，打印系统时间
-while 1:
+'''while 1:
     ps=subprocess.Popen(cmd,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
     ps.wait()
     RetStdOut,RetStdError=ps.communicate()
@@ -20,4 +20,11 @@ while 1:
         strTime=datetime.datetime.now()
         logging.debug(strTime)
         break
-    time.sleep(10)
+    time.sleep(10)'''
+cmd="""ls | wc -l"""
+cmd="""ps -ef | grep redis-server | grep -v grep | wc -l"""
+ps=subprocess.Popen(cmd,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+ps.wait()
+RetStdOut,RetStdError=ps.communicate()
+print("RetStdOut:"+RetStdOut)
+
