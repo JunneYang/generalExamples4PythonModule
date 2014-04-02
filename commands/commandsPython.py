2ed332084d3340891d9afba7e5f1263f088fcedb
JunneJun <yangjun03@baidu.com>
2014-04-02 16:16:08 +0800

    xcscsdcs

--- commands/commandsPython.py
+++ commands/commandsPython.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import commands
+import time
 
-cmdstr="""netstat -anop | grep 6379"""
-status,output=commands.getstatusoutput(cmdstr)
-if(status == 0):
-    print output
+if __name__ == "__main__":
+    while(1):
+        #cmdstr="""netstat -anop | grep 6379"""
+        cmdstr="""free | grep 'buffers/cache:' | awk -F' ' '{ print strftime("%H:%M:%S",systime())"\t",$3"\t",$4 }' >> meo.log"""
+        status,output=commands.getstatusoutput(cmdstr)
+        if(status == 0):
+            pass
+        else:
+            print("error")
+        time.sleep(60)
